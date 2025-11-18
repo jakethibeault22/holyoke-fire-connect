bda2330af4fb79efeeed76e453043808f6da6d81
Jake <jake.thibeault22@gmail.com>
2025-11-18 13:01:16 -0500

    Test persistence v2

--- src/App.jsx
+++ src/App.jsx
@@ -2574,14 +2574,14 @@ className="w-full p-2 border rounded"
 
       {/* Copyright footer */}
       <div className="fixed bottom-2 right-4 text-xs text-gray-400">
-        © Dylan Donicz 2025
+        © Jake Thibeault 2025
       </div>
     </div>
   );
 }  
       {/* Copyright footer */}
       <div className="fixed bottom-2 right-4 text-xs text-gray-400">
-        © Dylan Donicz 2025
+        © Jake Thibeault 2025
       </div>
     </div>
   );
