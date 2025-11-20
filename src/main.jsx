339169e67eae5f43af49fbc0264f364e2c496919
Jake <jake.thibeault22@gmail.com>
2025-11-20 11:23:36 -0500

    Add error logging for Samsung Internet debugging

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// Catch any errors
+window.onerror = function(msg, url, lineNo, columnNo, error) {
+  alert('Error: ' + msg + '\nLine: ' + lineNo);
+  return false;
+};
+
+window.addEventListener('unhandledrejection', function(event) {
+  alert('Unhandled rejection: ' + event.reason);
+});
+
+try {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} catch (error) {
+  alert('Render error: ' + error.message);
+  document.body.innerHTML = '<h1>Error: ' + error.message + '</h1>';
+}
