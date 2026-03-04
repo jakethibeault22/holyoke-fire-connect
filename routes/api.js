2549e5aeb3111630b82c03caf3dc3ba64ac295ca
Jake <jake.thibeault22@gmail.com>
2026-03-04 13:45:43 -0500

    fix cloudinary signed URL to use https

--- routes/api.js
+++ routes/api.js
@@ -1082,10 +1082,11 @@ router.get('/files/:id/download', async (req, res) => {
         const publicIdWithExt = urlParts[1].replace(/^v\d+\//, '');
 
         const signedUrl = cloudinary.url(publicIdWithExt, {
-          resource_type: 'raw',
-          sign_url: true,
-          expires_at: Math.floor(Date.now() / 1000) + 3600,
-        });
+  resource_type: 'raw',
+  sign_url: true,
+  expires_at: Math.floor(Date.now() / 1000) + 3600,
+  secure: true,
+});
 
         console.log('Signed URL:', signedUrl);
         return res.json({ url: signedUrl, filename: file.original_filename });
